8f3f84a005ca9b9167f0750b260a8c68056df6dc
talal739 <58117413+talal739@users.noreply.github.com>
2020-03-19 13:08:50 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+console.log('%c Onix bot ØªÙ… Ø¹Ù…Ù„ Ø§Ù„Ù…ÙˆÙ‚Ø¹ Ø¨ÙˆØ§Ø³Ø·Ø© Ù…Ø¨Ø±Ù…Ø¬ÙŠÙ† ', 'background: #222; color: #a29bfe');
+console.log('%c', 'background: #222; color: #fff');
+console.log('%c Onix bot ÙØ±ÙŠÙ‚', 'background: #222; color: #fff');
+console.log('%c Fireking ', 'background: #222; color: #a29bfe');
+console.log('%c iUltRaX ', 'background: #222; color: #a29bfe');
+console.log('%c .Ahmeeed ', 'background: #222; color: #a29bfe');
+console.log('%c ,YouseeeF ', 'background: #222; color: #a29bfe');
+console.log('%c E3SAR ', 'background: #222; color: #a29bfe');
+console.log('%c M7MD ', 'background: #222; color: #a29bfe');
+console.log('%c', 'background: #222; color: #FFC312');
+console.log('%c Onix bot Ø¬Ù…ÙŠØ¹ Ø§Ù„Ø­Ù‚ÙˆÙ‚ Ù…Ø­ÙÙˆØ¸Ø© Ù„Ø¯ÙŠ Ù…Ø¨Ø±Ù…Ø¬ÙŠÙ†  ', 'background: #222; color: #fff');
